763361a85ac785d79251a0edadf5eac69ebce832
“liutianhua” <1364593526@qq.com>
2024-03-19 22:05:35 +0800

    1

--- source/scripts/baidulianmeng.js
+++ source/scripts/baidulianmeng.js
@@ -1,1 +1,1 @@
-hexo.extend.injector.register('head_begin', '<meta name="baidu_union_verify" content="ce07df8f2b597b99b0b33e237b6f82a4">', 'default');
+hexo.extend.injector.register('head_begin', '<meta name="baidu_union_verify" content="3f24d6a838c890ccfd15c9198bfbeb51">', 'default');
